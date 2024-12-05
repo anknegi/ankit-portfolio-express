efa2b6fc3681b7632d6bd498f4b918c908fdf4a3
Ankit Negi <ankitnegi2008@gmail.com>
2024-12-05 09:13:01 +0530

    Added Model Controller logic for blog post

---
+++ model/User.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: [true, "A user must have a first name"],
+      },
+    lastname: {
+        type: String,
+        required: [true, "A user must have a last name"],
+    },
+    email: {
+        type: String,
+        required: [true, 'A user must have an email id'],
+        unique: [true, 'A user email must be unique'],
+        lowercase: true,
+        validate: [validator.isEmail, "Please enter a valid email"]
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }]
+})
+
+UserSchema.pre("save", async function(next) {
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+
+const User = mongoose.Model("User", UserSchema)
+
+module.exports = User
